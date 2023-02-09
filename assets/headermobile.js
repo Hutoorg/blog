d450b9a48ba0f4e709be3c106ed1543de5cba21f
Huto <92351427+Hutotpn@users.noreply.github.com>
2023-02-09 05:48:30 +0000

    Rename some folders and format doc

--- assets/headermobile.js
+++ assets/headermobile.js
@@ -4,9 +4,9 @@ const menuClose = doc.querySelector(".close");
 const overlay = doc.querySelector(".overlay");
 
 menuOpen.addEventListener("click", () => {
-  overlay.classList.add("overlay--active");
+	overlay.classList.add("overlay--active");
 });
 
 menuClose.addEventListener("click", () => {
-  overlay.classList.remove("overlay--active");
+	overlay.classList.remove("overlay--active");
 });
